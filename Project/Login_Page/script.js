6f34c9c2982dd7099d6b331a7f66d10eccc43427
Mohammad-Ayan <ayan2015786@gmail.com>
2024-02-08 19:00:46 +0530

    08-02-24

---
+++ Project/Login_Page/script.js
@@ -0,0 +1,11 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the form from submitting
+    // Add your login logic here
+    alert("Login clicked!");
+});
+
+document.getElementById("register-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the form from submitting
+    // Add your registration logic here
+    alert("Register clicked!");
+});
