f7fe69db6cba072f85033c9bea362bcfe2315902
Mohammad-Ayan <ayan2015786@gmail.com>
2024-03-16 16:24:44 +0530

    16-03-24

---
+++ Video55/script.js
@@ -0,0 +1,75 @@
+console.log("Hey this Tutorial 55")
+
+let a = 5;
+var b = 6;
+var c = "ayan";
+var _b = "habiba";
+// var 55a = "mohammad"   (not allowed)
+
+console.log(a + b + c);
+console.log(typeof a, typeof b, typeof c);
+
+const z = 2;
+
+// z = z+1;  (not allowed)
+
+{
+    let a = 23;
+    console.log(a);   // output:23
+}
+console.log(a);       // output:5
+
+let x = "harry";
+let f = 34;
+let j = undefined;
+let q = null;
+let n = true
+
+console.log(typeof n, typeof x, typeof f, typeof j, typeof q);
+
+// ------------------------ objects_in_js--------------------------
+
+let o = {
+    "name": "harry",
+    "salary code": 2356
+}
+
+console.log(o)
+
+o["salary code"] = 10000;
+console.log(o);
+
+//--------------------------practice_set-----------------------------
+
+//Q1
+var s = 30;
+
+console.log(k = s + 20);
+//Q2
+
+console.log(typeof k);
+//Q3
+const u = 34;
+// v = u+1;
+// console.log(v);
+
+//Q4
+const v = u - 12;
+console.log(v);
+//Q5
+let dictionary = {
+    "Serene": "Calm",
+
+    "Eloquent": "Fluentin speaking or writing",
+
+    "Vivid": "Producing powerful clear images in the mind",
+
+    "Oblique":  "not straightforward",
+
+    "Nurture": "To care for and encourage the growth or development of someone or something",
+
+    "Whimsy": "Playfully quaint or fanciful behavior or humor"
+};
+console.log(dictionary);
+
+
