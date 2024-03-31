faf4abd55f6f6686ad50c78181ce719fb263e834
Mohammad-Ayan <ayan2015786@gmail.com>
2024-03-31 07:26:51 +0530

    31-03-24

---
+++ Video56/index.js
@@ -0,0 +1,48 @@
+console.log("Hello I am conditional tutorial")
+
+let age = 17;
+// let grace = 2;
+
+// console.log(age+grace);
+// console.log(age-grace);
+// console.log(age*grace);
+// console.log(age/grace);
+// console.log(age%grace);
+// console.log(age**grace);
+// if((age + grace)>18){
+//     console.log("You can Drive");
+// }
+
+// else{
+//     console.log("You can't Drive")
+// }
+
+if (age > 18) {
+    console.log("You can Drive");
+}
+
+else if (age == 0) {
+    console.log("Are you kidding")
+}
+
+else {
+    console.log("You can't Drive")
+}
+
+
+//  ternary operator
+a = 6;
+b = 9;
+
+let c = a > b ? (a - b) : (a + b);
+
+//--------------------------------------Prac_Set-2---------------------------------------------------
+
+//Q1
+age = 10 
+
+if (age>10) {
+    
+} else {
+    
+}
