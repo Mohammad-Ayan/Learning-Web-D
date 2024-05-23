448b5525836eb7ea73baaad6b1f8a9954fb538a1
Mohammad-Ayan <ayan2015786@gmail.com>
2024-05-24 00:03:36 +0530

    24-05-24

--- Video60-JS/index.js
+++ Video60-JS/index.js
@@ -13,3 +13,14 @@ let friend = "Tahir"
 
 console.log("My name is: "+ real_name + " My friend name is: " + friend)
 console.log(`My name is: ${real_name} My friend name is: ${friend}`)  //Template_Literals
+
+let b = "Arham"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.slice(1, 4))
+
+console.log(b.replace("ham", "zoo"))
+console.log(b.concat(" ",a, " are both colleague"))
+
+console.log(b.startsWith("rr"))  //false
+console.log(b.endsWith("am"))   //true
