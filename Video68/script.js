25d58952063d34f43ccccd9df878401d79312609
Mohammad-Ayan <ayan2015786@gmail.com>
2024-05-30 12:45:16 +0530

    Selectors_JS

---
+++ Video68/script.js
@@ -0,0 +1,17 @@
+console.log("Remember Me I will Remember You")
+
+// let boxes = document.getElementsByClassName("box")
+
+// console.log(boxes)
+
+// boxes[4].style.backgroundColor = "red"
+
+// let boxes = document.getElementById("trust").style.backgroundColor = "red"
+
+// document.querySelector(".box").style.backgroundColor = ("cyan")
+
+document.querySelectorAll(".box").forEach(element => {
+    element.style.backgroundColor = ("pink")
+});
+
+// document.querySelector(".container").contains(document(e[2]))
