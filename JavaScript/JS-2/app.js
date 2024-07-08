7e79d233bc913dff52e1014652157b89bb006f53
Mohammad-Ayan <ayan2015786@gmail.com>
2024-07-08 08:53:38 +0530

    07-07-24

---
+++ JavaScript/JS-2/app.js
@@ -0,0 +1,45 @@
+// function hello() {
+//     console.log("hello")
+// }
+
+// function printName() {
+//     console.log("Muhammad Ayan")
+// }
+
+// function print_1to9() {
+//     for (let i = 1; i <= 9; i++) {
+//         console.log(i)
+//     }
+// }
+
+// function isAdult(){
+//     let age = 28;
+//     if(age < 18){
+//         console.log("It's Minor")
+//     }
+//     else{
+//         console.log("Adult")
+//     }
+// }
+
+// print_1to9();
+// printName();
+// hello();
+// isAdult();
+
+//---Q1(Poem)
+// function printPoem(){
+//     console.log("Jonny Jonny")
+//     console.log("Yes Papa")
+// }
+
+// printPoem();
+
+//---Q2(Dice)
+
+function dice(){
+        let rand = Math.floor(Math.random() * 6)+1;
+        console.log(rand);
+}
+
+dice();
