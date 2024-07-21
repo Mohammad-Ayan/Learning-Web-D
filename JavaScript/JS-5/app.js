8ba3357b804360d110acf203be10bbaefe4b3a5a
Mohammad-Ayan <ayan2015786@gmail.com>
2024-07-21 08:19:04 +0530

    .

---
+++ JavaScript/JS-5/app.js
@@ -0,0 +1,22 @@
+//  --------------QUERY SELECOR-----------
+
+document.querySelector('p');  //Selects first p element
+document.querySelector('#myId');  //Selects first element with id = myId
+document.querySelector('myClass');  //Selects first element with class = myClass
+
+document.querySelectorAll("p"); //Selects all p elements
+ 
+// style Attributes
+
+let links = document.querySelectorAll(".box ");
+
+
+for ( link of links) {
+    links.style.color = "yellow"; // inline style
+}
+
+//OR
+
+// for(let i=0; i<links.length; i++){
+//     links[i].style.color = "yellow";
+// }
