e51e888141579ca30ad996ce85793c8f6ead75f5
Mohammad-Ayan <ayan2015786@gmail.com>
2024-08-14 08:34:16 +0530

    .

--- JavaScript/JS-5/app.js
+++ JavaScript/JS-5/app.js
@@ -57,3 +57,59 @@ div.append(para2);
 div.classList.add("box");
 
 document.querySelector("body").append(div);
+
+//-----------------------------Assignment Ques-----------------------
+
+//Qs 1. Create a new input and button element on the page using JavaScript only.
+// Set the text of button to "Click me
+
+let inputElement = document.createElement("input");
+// inputElement.type = "text";
+let buttonElement = document.createElement("button");
+buttonElement.innerText = "Click me";
+
+document.querySelector("body").append(inputElement);
+document.querySelector("body").append(buttonElement);
+
+
+// Qs2. Add following attributes to the element :
+// Change placeholder value of input to "username"
+// Change the id of button to "btn'
+
+let input = document.createElement("input");
+input.type = "text";
+input.placeholder = "username"
+
+let button = document.createElement("button");
+button.innerText = "Click me";
+button.id = "btn";
+
+document.querySelector("body").append(input);
+document.querySelector("body").append(button);
+
+//OR
+buttonElement.setAttribute("id", "btn");
+inputElement.setAttribute("placeholder", "username (by Apna Collge)");
+
+// Qs3. Access the btn using the querySelector and button id.
+//  Change the button background color to blue and text color to white.
+
+
+let btn = document.querySelector("#btn");
+btn.classList.add("btnStyle")
+
+// Qs4. Create an hl element on the page and set its text to "DOM Practice" underlined. 
+// Change its color to purple.
+
+// let h1 = document.createElement("h1");
+// h1.innerHTML = "<u>DOM Practice</u>"
+
+// document.querySelector("body").append(h1);
+    
+// Qs5. Create a p tag on the page and set its text to "Apna College Delta Practice",
+// where Delta is bold.
+
+// let p = createElement("p");
+// p.innerText = "Apna College <b>Delta</b> Practice"
+
+// document.querySelector("body").append(p);
