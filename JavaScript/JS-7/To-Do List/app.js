d6f98ec779da950d91abbb971f358ce968091f46
Mohammad-Ayan <ayan2015786@gmail.com>
2024-09-08 15:38:00 +0530

    07_09_24

--- JavaScript/JS-7/To-Do List/app.js
+++ JavaScript/JS-7/To-Do List/app.js
@@ -12,21 +12,13 @@ btn.addEventListener("click", function () {
 
     // Append the delete button to the newly created list item
     item.appendChild(delBtn)
-    // Append the new list item (with delete button) to the ul element
+    // Append the new list item (with delete button)to the ul element
     ul.appendChild(item)
     // Clear the input field after the item is added
     inp.value = "";
 })
 
 // for deleting the task
-// let delBtns = document.querySelectorAll(".delete");
-// for (delBtn of delBtns) {
-//     delBtn.addEventListener("click", function () {
-//         let par = this.parentElement;
-//         par.remove();  // Remove the parent element
-//     })
-// }
-
 ul.addEventListener("click", function (event) {
     if (event.target.nodeName == "BUTTON") {
         let listItem = event.target.parentElement;
