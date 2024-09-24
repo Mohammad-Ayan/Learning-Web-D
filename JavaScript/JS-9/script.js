3318ac57a27bd8bcdcd957ea55cb5b925402eb82
Mohammad-Ayan <ayan2015786@gmail.com>
2024-09-24 07:11:50 +0530

    .

---
+++ JavaScript/JS-9/script.js
@@ -0,0 +1,19 @@
+//---------------Async Function-------------
+
+async function greet() {
+    // throw "404 page not found";
+    return "hello!";
+}
+
+greet()
+.then(()=>{
+    console.log("promise was resolved");
+    
+})
+.catch((err)=>{
+console.log("promise was rejected with err", err);
+})
+
+let demo = async () => {
+    return 5;
+}
