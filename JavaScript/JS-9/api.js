b1ac432619dc79e3d3ed11274db80404c0247260
Mohammad-Ayan <ayan2015786@gmail.com>
2024-10-01 23:59:15 +0530

    API / JSON

--- JavaScript/JS-9/api.js
+++ JavaScript/JS-9/api.js
@@ -10,13 +10,43 @@ let student = {
 }
 
 //------------------API request---------------
+
 let url = "https://catfact.ninja/fact";
 
-fetch(url)
+fetch(url) // promise
     .then((res) => {
-        console.log(res);
-        res.json()  // make it readeable and returns promise
+        return res.json();  // parse 
+    })
+    .then((data1) => {
+        console.log("data1: ", data1.fact);
+        return fetch(url);
+    })
+    .then((res) => {
+        return res.json();  // parse 
+    })
+    .then((data2) => {
+        console.log("data2: ", data2.fact);
     })
     .catch((err) => {
         console.log("ERROR", err);
     });
+
+//------------------Fetch with Async-Await ---------------
+
+let url1 = "https://catfact.ninja/fact";
+
+async function getFacts() {
+    try {
+        let res = await fetch(url1);
+        let data = await res.json();
+        console.log(data.fact);
+
+        let res2 = await fetch(url1);
+        let data2 = await res.json();
+        console.log(data2.fact);
+
+    } catch (err) {
+        console.log("error", err);
+    }
+}
+
