59215c374ff60d9de066367ef12e047b773deee9
Mohammad-Ayan <ayan2015786@gmail.com>
2024-10-01 19:10:39 +0530

    .

--- JavaScript/JS-9/api.js
+++ JavaScript/JS-9/api.js
@@ -1,10 +1,22 @@
-let jsonRes = 
-`{"fact": "Approximately 1/3 of cat owner thinks that their pets are able to read their minds.","length":78}`;
+let jsonRes =
+    `{"fact": "Approximately 1/3 of cat owner thinks that their pets are able to read their minds.","length":78}`;
 
 let validRes = JSON.parse(jsonRes);
 console.log(validRes.fact);
 
 let student = {
-    name : "ayan",
+    name: "ayan",
     marks: "85"
 }
+
+//------------------API request---------------
+let url = "https://catfact.ninja/fact";
+
+fetch(url)
+    .then((res) => {
+        console.log(res);
+        res.json()  // make it readeable and returns promise
+    })
+    .catch((err) => {
+        console.log("ERROR", err);
+    });
