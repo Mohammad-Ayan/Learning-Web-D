fe692a9113fb05f97d129571360d287e226be40f
Mohammad-Ayan <ayan2015786@gmail.com>
2024-10-03 08:25:17 +0530

    02-10-24

--- JavaScript/JS-10/app.js
+++ JavaScript/JS-10/app.js
@@ -1,5 +1,5 @@
 //-------------request using Axios-------------
-
+//---Cat_API---
 let btn = document.querySelector("button");
 
 btn.addEventListener("click", async () => {
@@ -20,4 +20,26 @@ async function getFacts() {
         return "No Fact Found"; 
     }
 }
- 
+
+//---Dog_API---
+let url1 = "https://dog.ceo/api/breeds/image/random"
+
+let btn2 = document.querySelector("button");
+
+btn.addEventListener("click", async () => {
+    let link =  await getImage();
+let img = document.querySelector("#result");
+img.setAttribute("src", link);
+});
+
+
+async function getImage() {
+    try {
+        let res = await axios.get(url1);
+        return res.data.message;    // without parsing using Axios
+
+    } catch (err) {
+        console.log("error", err);
+        return "No Image Found"; 
+    }
+}
