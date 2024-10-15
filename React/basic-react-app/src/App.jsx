02f441a75cbfa3fe8a927a47951ab1dd7d1de75e
Mohammad-Ayan <ayan2015786@gmail.com>
2024-10-15 16:14:01 +0530

    .

--- React/basic-react-app/src/App.jsx
+++ React/basic-react-app/src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css"
+import Title from "./Title.jsx";
+import Product from "./Product.jsx";
+
+
+
+function Description() {
+  return <h3>I'm the Description</h3>
+}
 
 function App() {
- return 
+  return (
+    <>
+      <Product />
+      <Product />
+    </>
+  );
 }
+
 
 export default App
---
+++ React/basic-react-app/src/Title.jsx
@@ -0,0 +1,5 @@
+function Title() {
+    return <h1>I'm the Title</h1>
+}
+
+export default Title;
