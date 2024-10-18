2183da18538fd7c102e63f08549745e770051582
Mohammad-Ayan <ayan2015786@gmail.com>
2024-10-18 12:17:40 +0530

    Ok

---
+++ React/activity/src/Price.jsx
@@ -0,0 +1,10 @@
+export default function Price() {
+    return (
+        <div>
+            <p>oldPrice</p>
+            <p>newPrice</p>
+        </div>
+    );
+}   
+
+
---
+++ React/activity/src/Product.jsx
@@ -0,0 +1,15 @@
+import "./Product.css";
+import Price from "./Price.jsx";
+
+function Product ({ title, price, features }) {
+    return(
+        <div className="Product">
+            <p>Title</p>
+            <p>Description</p>
+            <Price/>
+        </div>
+    );
+}
+ 
+
+export default Product;  
