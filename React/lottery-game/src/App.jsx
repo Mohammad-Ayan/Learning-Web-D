7796c2e4791677bf9a8e2a47a1ff96a243b9157f
Mohammad-Ayan <ayan2015786@gmail.com>
2024-10-31 01:30:54 +0530

    .,.,,

---
+++ React/lottery-game/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Lottery from './lottery'
+
+
+function App() {
+
+
+  return (
+    <>
+     <Lottery />
+    </>
+  )
+}
+
+export default App
